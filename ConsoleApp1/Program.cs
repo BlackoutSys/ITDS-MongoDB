97a16ab87cfcd771347c56a6c799a080d98f301c
Sebastian Pietrzak <sebastianp.pietrzak@gmail.com>
2020-06-18 09:13:17 +0200

    Initial app

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new MongoClient("mongodb+srv://ITDS_USER:ITDSPASS@testcluster-ttkfd.mongodb.net/sample_mflix?retryWrites=true&w=majority");
+            var database = client.GetDatabase("sample_mflix");
+
+            var filterBuilder = new FilterDefinitionBuilder<BsonDocument>();
+            var filter = filterBuilder.Lte("year", 1983);
+            using (IAsyncCursor<BsonDocument> documents = database.GetCollection<BsonDocument>("movies").FindSync(filter))
+            {
+                while (documents.MoveNext())
+                {
+                    foreach(var doc in documents.Current)
+                    {
+                        Console.WriteLine(doc.ToJson());
+                    }
+                }
+            }
+        } 
+    }
+}
